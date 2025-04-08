9838e6f06901f79d9745c319b0136601799b233e
Van Tran <van.tran@asnet.com.vn>
2025-04-08 15:47:08 +0700

    Configure TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+			pwaAssets: {
+				disabled: false,
+				config: true
+			},
 
-    manifest: {
-      name: 'Weather',
-      short_name: 'Weather',
-      description: 'This is the weather forecast app.',
-      theme_color: '#391A49',
-    },
+			manifest: {
+				name: 'Weather',
+				short_name: 'Weather',
+				description: 'This is the weather forecast app.',
+				theme_color: '#391A49'
+			},
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true
+			},
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+			devOptions: {
+				enabled: false,
+				navigateFallback: 'index.html',
+				suppressWarnings: true,
+				type: 'module'
+			}
+		})
+	]
+});
