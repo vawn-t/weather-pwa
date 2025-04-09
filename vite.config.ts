1439166f596e02cd5dea0bd3c1df1d821b82dae7
Van Tran <van.tran@asnet.com.vn>
2025-04-09 11:31:57 +0700

    Set up app icons and font family\

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,83 @@ export default defineConfig({
 				name: 'Weather',
 				short_name: 'Weather',
 				description: 'This is the weather forecast app.',
+				icons: [
+					{
+						src: '/icons/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any'
+					},
+					{
+						src: '/icons/maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					},
+					{
+						src: '/icons/pwa-384x384.png',
+						sizes: '384x384',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any'
+					},
+					{
+						src: '/icons/maskable-icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable'
+					},
+					{
+						src: '/icons/apple-touch-icon-180x180.png',
+						sizes: '180x180',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/pwa-144x144.png',
+						sizes: '144x144',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/pwa-128x128.png',
+						sizes: '128x128',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/apple-touch-icon-120x120.png',
+						sizes: '120x120',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/pwa-96x96.png',
+						sizes: '96x96',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/pwa-72x72.png',
+						sizes: '72x72',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/pwa-48x48.png',
+						sizes: '48x48',
+						type: 'image/png'
+					}
+				],
+				display: 'standalone',
+				orientation: 'portrait',
+				lang: 'en',
+				scope: '/',
+				start_url: '/',
+				background_color: '#391A49',
 				theme_color: '#391A49'
 			},
 
@@ -31,7 +108,7 @@ export default defineConfig({
 			},
 
 			devOptions: {
-				enabled: false,
+				enabled: true,
 				navigateFallback: 'index.html',
 				suppressWarnings: true,
 				type: 'module'
