19dbcfe05d080c6c6b510c949df156557d9ef55e
Van Tran <van.tran@asnet.com.vn>
2025-04-15 17:07:31 +0700

    Add PWA icons and update icon paths in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,18 @@ import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    basicSsl(),
+    // basicSsl(),
     tailwindcss(),
 
     VitePWA({
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
-
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
 
       includeAssets: ['**/*'],
 
@@ -29,71 +23,71 @@ export default defineConfig({
         description: 'This is the weather forecast app.',
         icons: [
           {
-            src: 'icons/pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any',
           },
           {
-            src: 'icons/maskable-icon-512x512.png',
+            src: '/maskable-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
           },
           {
-            src: 'icons/pwa-384x384.png',
+            src: '/pwa-384x384.png',
             sizes: '384x384',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any',
           },
           {
-            src: 'icons/maskable-icon-192x192.png',
+            src: '/maskable-icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable',
           },
           {
-            src: 'icons/apple-touch-icon-180x180.png',
+            src: '/apple-touch-icon-180x180.png',
             sizes: '180x180',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-144x144.png',
+            src: '/pwa-144x144.png',
             sizes: '144x144',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-128x128.png',
+            src: '/pwa-128x128.png',
             sizes: '128x128',
             type: 'image/png',
           },
           {
-            src: 'icons/apple-touch-icon-120x120.png',
+            src: '/apple-touch-icon-120x120.png',
             sizes: '120x120',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-96x96.png',
+            src: '/pwa-96x96.png',
             sizes: '96x96',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-72x72.png',
+            src: '/pwa-72x72.png',
             sizes: '72x72',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-64x64.png',
+            src: '/pwa-64x64.png',
             sizes: '64x64',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-48x48.png',
+            src: '/pwa-48x48.png',
             sizes: '48x48',
             type: 'image/png',
           },
@@ -108,7 +102,6 @@ export default defineConfig({
       },
 
       workbox: {
-        globPatterns: ['**/*'],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
         skipWaiting: true,
