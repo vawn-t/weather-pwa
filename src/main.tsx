7e5301488a7a31d085325b77f43e1413a37fddd2
Van Tran <van.tran@asnet.com.vn>
2025-04-15 16:11:33 +0700

    Remove service worker registration and associated prompt from main.tsx; clean up tsconfig by removing unused types

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import { registerSW } from 'virtual:pwa-register';
 
 import App from './App.tsx';
 
 import './index.css';
-
-// add this to prompt for a refresh
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('New content available. Reload?')) {
-      updateSW(true);
-    }
-  },
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
