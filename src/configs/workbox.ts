21e9826c7df1fd724bbdfd13724b5a7e1676542a
Van Tran <van.tran@asnet.com.vn>
2025-04-26 13:59:46 +0700

    Improve splash screens for all ios devices

--- src/configs/workbox.ts
+++ src/configs/workbox.ts
@@ -1,5 +1,12 @@
-import { CACHE_NAMES } from 'src/constants/cache';
 import { GenerateSWOptions } from 'workbox-build';
+
+const CACHE_NAMES = {
+  WEATHER_API: 'weather-api-cache',
+  SEARCH_WEATHER_API: 'search-weather-api-cache',
+  STATIC_ASSETS: 'static-assets-cache',
+  APP_SHELL: 'app-shell-cache',
+  STATIC_RESOURCES: 'static-resources-cache',
+};
 
 const A_MONTH = 2592000;
 const A_DAY = 86400;
--- src/constants/cache.ts
+++
@@ -1,7 +0,0 @@
-export const CACHE_NAMES = {
-  WEATHER_API: 'weather-api-cache',
-  SEARCH_WEATHER_API: 'search-weather-api-cache',
-  STATIC_ASSETS: 'static-assets-cache',
-  APP_SHELL: 'app-shell-cache',
-  STATIC_RESOURCES: 'static-resources-cache',
-};
